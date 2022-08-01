19ab68bccdebecfff00505742fa5f61e72465852
cerenault <cesar.renault@etudiant.univ-lr.fr>
2022-08-01 12:35:54 +0200

    improve: netlify function

--- functions/functionsTest.ts
+++ functions/functionsTest.ts
@@ -1,8 +1,43 @@
+import {ApolloClient, InMemoryCache, createHttpLink, gql} from '@apollo/client';
+import {setContext} from '@apollo/client/link/context';
+
 exports.handler = async (event: any, context: any) => {
+    const API_URI = process.env.REACT_APP_API_URI;
+    const API_KEY = process.env.REACT_APP_API_KEY;
+
+    const httpLink = createHttpLink({
+        uri: API_URI,
+    });
+
+    const authLink = setContext((_, {headers}) => {
+        // get the authentication token from local storage if it exists
+        const token = API_KEY;
+        // return the headers to the context so httpLink can read them
+        return {
+            headers: {
+                ...headers,
+                authorization: token ? token : '',
+            },
+        };
+    });
+
+    const client = new ApolloClient({
+        link: authLink.concat(httpLink),
+        cache: new InMemoryCache(),
+    });
     try {
+        const res = await client.query({
+            query: gql`
+                query {
+                    getAllBrands {
+                        name
+                    }
+                }
+            `,
+        });
         return {
             statusCode: 200,
-            body: JSON.stringify({message: 'hello world'}),
+            body: JSON.stringify({message: res.data.getAllBrands}),
         };
     } catch (e) {
         console.log(e);
--- src/index.tsx
+++ src/index.tsx
@@ -3,32 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {ApolloClient, InMemoryCache, createHttpLink} from '@apollo/client';
-import {setContext} from '@apollo/client/link/context';
-
-const API_URI = process.env.REACT_APP_API_URI;
-const API_KEY = process.env.REACT_APP_API_KEY;
-
-const httpLink = createHttpLink({
-    uri: API_URI,
-});
-
-const authLink = setContext((_, {headers}) => {
-    // get the authentication token from local storage if it exists
-    const token = API_KEY;
-    // return the headers to the context so httpLink can read them
-    return {
-        headers: {
-            ...headers,
-            authorization: token ? token : '',
-        },
-    };
-});
-
-export const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-});
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
