e7c20dcc3e60a0e0c681410cfca522b6ad036c90
cerenault <cesar.renault@etudiant.univ-lr.fr>
2022-08-01 10:21:32 +0200

    add: netlify function test

--- src/index.tsx
+++ src/index.tsx
@@ -34,17 +34,27 @@ const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 
-client
-    .query({
-        query: gql`
-            query {
-                getAllBrands {
-                    name
-                }
-            }
-        `,
-    })
-    .then((e) => console.log('res : ', e));
+exports.handler = async (event: any, context: any) => {
+    try {
+        client
+            .query({
+                query: gql`
+                    query {
+                        getAllBrands {
+                            name
+                        }
+                    }
+                `,
+            })
+            .then((e) => console.log('res : ', e));
+    } catch (e) {
+        console.log(e);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({error: 'Failed fetching'}),
+        };
+    }
+};
 
 root.render(
     <React.StrictMode>
