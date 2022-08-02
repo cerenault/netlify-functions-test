87295fe154885756c60f10e3695f4c50723ac8de
cerenault <cesar.renault@etudiant.univ-lr.fr>
2022-08-02 12:49:32 +0200

    improve: import apollo client

--- functions/functionsTest.ts
+++ functions/functionsTest.ts
@@ -1,38 +1,40 @@
-import {gql, ApolloClient, HttpLink, InMemoryCache} from '@apollo/client';
-import {setContext} from '@apollo/client/link/context';
+import {gql} from '@apollo/client';
+// import {ApolloClient, HttpLink, InMemoryCache} from '@apollo/client';
+// import {setContext} from '@apollo/client/link/context';
 import fetch from 'cross-fetch';
+import {client} from '../graphql/config';
 
 // const brandsHandler: Handler = async (event, context) => {
 exports.handler = async (event, context) => {
     console.log('----> EVENT : ', event, 'BODY : ', event.body);
     console.log('----> CONTEXT : ', context);
     try {
-        const API_URI = process.env.REACT_APP_API_URI;
+        // const API_URI = process.env.REACT_APP_API_URI;
         const API_KEY = process.env.REACT_APP_API_KEY
             ? process.env.REACT_APP_API_KEY
             : '';
         console.log('API KEY : ', API_KEY);
 
         /**************** WITH APOLLO CLIENT ****************/
-        const httpLink = new HttpLink({
-            uri: API_URI,
-            fetch,
-        });
-        const authLink = setContext((_, {headers}) => {
-            // get the authentication token from local storage if it exists
-            const token = API_KEY;
-            // return the headers to the context so httpLink can read them
-            return {
-                headers: {
-                    ...headers,
-                    authorization: token ? token : '',
-                },
-            };
-        });
-        const client = new ApolloClient({
-            link: authLink.concat(httpLink),
-            cache: new InMemoryCache(),
-        });
+        // const httpLink = new HttpLink({
+        //     uri: API_URI,
+        //     fetch,
+        // });
+        // const authLink = setContext((_, {headers}) => {
+        //     // get the authentication token from local storage if it exists
+        //     const token = API_KEY;
+        //     // return the headers to the context so httpLink can read them
+        //     return {
+        //         headers: {
+        //             ...headers,
+        //             authorization: token ? token : '',
+        //         },
+        //     };
+        // });
+        // const client = new ApolloClient({
+        //     link: authLink.concat(httpLink),
+        //     cache: new InMemoryCache(),
+        // });
 
         const results = await client.query({
             query: gql`
